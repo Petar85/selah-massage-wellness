471949cecc170b3275f41a69271fb97ac73f3732
Peter Pan <petarzivkovic85@gmail.com>
2023-01-10 18:35:02 -0800

    created cart component with number of items

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Link from "next/link";
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import toast from "react-icons/toast";
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from "../lib/client";
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartitems, setShowCart } = useStateContext();
+
   return (
-    <div>Cart</div>
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button type="button" className="cart-heading" onClick={() => 
+        setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+      </div>
+    </div>
   )
 }
 
