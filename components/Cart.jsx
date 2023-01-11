bac54cad2713a3e66d2ff5d48d5b36dff33c91a9
Peter Pan <petarzivkovic85@gmail.com>
2023-01-11 00:19:01 -0800

    type error in cart component it does not show up

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from "next/link";
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from "react-icons/ai";
 import { TiDeleteOutline } from "react-icons/ti";
-import toast from "react-icons/toast";
+// import toast from "react-icons/toast";
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from "../lib/client";
 
@@ -19,6 +19,30 @@ const Cart = () => {
           <span className="heading">Your Cart</span>
           <span className="cart-num-items">({totalQuantities} items)</span>
         </button>
+        {cartitems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button type="button" onClick={() => setShowCart(false)} className="btn">Continue Shopping</button>
+            </Link>
+          </div>
+        )}
+        <div className="product-container">
+          {cartitems.length >=1 && cartitems.map((item) => (
+            <div className="product" key={item._id}>
+              <img src={urlFor(item?.image[0])}
+              className="cart-product-image" />
+              <div className="item-desc">
+                <div className="flex top">
+                  <h5>{item.name}</h5>
+                  <h4>{item.price}</h4>
+                </div>
+              </div>
+            </div>
+
+          ))}
+        </div>
       </div>
     </div>
   )
